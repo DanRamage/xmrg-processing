102a14036585a949a1ff8db329fa80a1881c55d8
DANRAMAGE <ChiefDan@gmail.com>
2024-08-30 11:45:27 -0400

    Bump version.

--- xmrgprocessing/xmrgdatasaver/nexrad_xenia_saver.py
+++ xmrgprocessing/xmrgdatasaver/nexrad_xenia_saver.py
@@ -54,106 +54,109 @@ class nexrad_xenia_sqlite_saver(precipitation_saver):
         return
 
     def save(self, xmrg_results_data):
-        for boundary_name, boundary_results in xmrg_results_data.get_boundary_data():
-            '''
-            if self.writePrecipToKML and xmrg_results_data.get_boundary_grid(boundary_name) is not None:
-                self.write_boundary_grid_kml(boundary_name, xmrg_results_data)
+        try:
+            for boundary_name, boundary_results in xmrg_results_data.get_boundary_data():
+                '''
+                if self.writePrecipToKML and xmrg_results_data.get_boundary_grid(boundary_name) is not None:
+                    self.write_boundary_grid_kml(boundary_name, xmrg_results_data)
+    
+                if self.kmlTimeSeries:
+                    self.kml_time_series.append(xmrg_results_data)
+                '''
+                platform_handle = "nws.%s.radarcoverage" % (boundary_name)
+                self._logger.info(f"Saving platform: {platform_handle} {xmrg_results_data.datetime}")
+                if self._check_exists:
+                    self.check_exists(platform_handle, xmrg_results_data)
+                lat = 0.0
+                lon = 0.0
 
-            if self.kmlTimeSeries:
-                self.kml_time_series.append(xmrg_results_data)
-            '''
-            platform_handle = "nws.%s.radarcoverage" % (boundary_name)
-            self._logger.info(f"Saving platform: {platform_handle} {xmrg_results_data.datetime}")
-            if self._check_exists:
-                self.check_exists(platform_handle, xmrg_results_data)
-            lat = 0.0
-            lon = 0.0
+                avg = boundary_results['weighted_average']
+                if avg != None:
+                    if avg > 0.0 or self._save_all_precip_values:
+                        if avg != -9999:
 
-            avg = boundary_results['weighted_average']
-            if avg != None:
-                if avg > 0.0 or self._save_all_precip_values:
-                    if avg != -9999:
-
-                        if self._add_sensors:
-                            self._xenia_db.addNewSensor('precipitation_radar_weighted_average', 'mm',
-                                                        platform_handle,
-                                                        1,
-                                                        0,
-                                                        1, None, True)
-                        # Build a dict of m_type and sensor_id for each platform to make the inserts
-                        # quicker.
-                        if platform_handle not in self.sensor_ids:
-                            try:
-                                platform_info = self._xenia_db.session.query(platform) \
-                                    .filter(platform.platform_handle == platform_handle) \
-                                    .one()
-                            except Exception as e:
-                                self._logger.exception(e)
-                            else:
-                                m_type_id = self._xenia_db.mTypeExists('precipitation_radar_weighted_average',
-                                                                       'mm')
-                                sensor_id = self._xenia_db.sensorExists('precipitation_radar_weighted_average',
-                                                                        'mm',
-                                                                        platform_handle, 1)
-                                self.sensor_ids[platform_handle] = {
-                                    'latitude': platform_info.fixed_latitude,
-                                    'longitude': platform_info.fixed_longitude,
-                                    'm_type_id': m_type_id,
-                                    'sensor_id': sensor_id}
-
-                        # Add the avg into the multi obs table. Since we are going to deal with the hourly data for the radar and use
-                        # weighted averages, instead of keeping lots of radar data in the radar table, we calc the avg and
-                        # store it as an obs in the multi-obs table.
-                        add_obs_start_time = time.time()
-                        try:
-                            db_rec = multi_obs(
-                                row_entry_date=self.row_entry_date,
-                                platform_handle=platform_handle,
-                                sensor_id=self.sensor_ids[platform_handle]['sensor_id'],
-                                m_type_id=self.sensor_ids[platform_handle]['m_type_id'],
-                                m_date=xmrg_results_data.datetime,
-                                m_lon=self.sensor_ids[platform_handle]['latitude'],
-                                m_lat=self.sensor_ids[platform_handle]['longitude'],
-                                m_value=avg
-                            )
-                            try:
-                                self._xenia_db.session.add(db_rec)
-                                self._xenia_db.session.commit()
-                            # Trying to add record that already exists.
-                            except exc.IntegrityError as e:
-                                self._xenia_db.session.rollback()
-                                self._logger.error("Record already exists.")
+                            if self._add_sensors:
+                                self._xenia_db.addNewSensor('precipitation_radar_weighted_average', 'mm',
+                                                            platform_handle,
+                                                            1,
+                                                            0,
+                                                            1, None, True)
+                            # Build a dict of m_type and sensor_id for each platform to make the inserts
+                            # quicker.
+                            if platform_handle not in self.sensor_ids:
                                 try:
-                                    add_obs_start_time = time.time()
-                                    db_update_rec = update(multi_obs) \
-                                        .values({"m_value": avg}) \
-                                        .where(multi_obs.platform_handle == platform_handle) \
-                                        .where(multi_obs.m_type_id == self.sensor_ids[platform_handle]['m_type_id']) \
-                                        .where(multi_obs.sensor_id == self.sensor_ids[platform_handle]['sensor_id']) \
-                                        .where(multi_obs.m_date == xmrg_results_data.datetime)
-
-                                    self._xenia_db.connection.execute(db_update_rec)
+                                    platform_info = self._xenia_db.session.query(platform) \
+                                        .filter(platform.platform_handle == platform_handle) \
+                                        .one()
                                 except Exception as e:
                                     self._logger.exception(e)
                                 else:
-                                    self._logger.debug(
-                                        f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} updated "
-                                        f"weighted avg: {avg} in {time.time() - add_obs_start_time} seconds.")
+                                    m_type_id = self._xenia_db.mTypeExists('precipitation_radar_weighted_average',
+                                                                           'mm')
+                                    sensor_id = self._xenia_db.sensorExists('precipitation_radar_weighted_average',
+                                                                            'mm',
+                                                                            platform_handle, 1)
+                                    self.sensor_ids[platform_handle] = {
+                                        'latitude': platform_info.fixed_latitude,
+                                        'longitude': platform_info.fixed_longitude,
+                                        'm_type_id': m_type_id,
+                                        'sensor_id': sensor_id}
 
-                        except Exception as e:
-                            self._logger.exception(e)
+                            # Add the avg into the multi obs table. Since we are going to deal with the hourly data for the radar and use
+                            # weighted averages, instead of keeping lots of radar data in the radar table, we calc the avg and
+                            # store it as an obs in the multi-obs table.
+                            add_obs_start_time = time.time()
+                            try:
+                                db_rec = multi_obs(
+                                    row_entry_date=self.row_entry_date,
+                                    platform_handle=platform_handle,
+                                    sensor_id=self.sensor_ids[platform_handle]['sensor_id'],
+                                    m_type_id=self.sensor_ids[platform_handle]['m_type_id'],
+                                    m_date=xmrg_results_data.datetime,
+                                    m_lon=self.sensor_ids[platform_handle]['latitude'],
+                                    m_lat=self.sensor_ids[platform_handle]['longitude'],
+                                    m_value=avg
+                                )
+                                try:
+                                    self._xenia_db.session.add(db_rec)
+                                    self._xenia_db.session.commit()
+                                # Trying to add record that already exists.
+                                except exc.IntegrityError as e:
+                                    self._xenia_db.session.rollback()
+                                    self._logger.error("Record already exists.")
+                                    try:
+                                        add_obs_start_time = time.time()
+                                        db_update_rec = update(multi_obs) \
+                                            .values({"m_value": avg}) \
+                                            .where(multi_obs.platform_handle == platform_handle) \
+                                            .where(multi_obs.m_type_id == self.sensor_ids[platform_handle]['m_type_id']) \
+                                            .where(multi_obs.sensor_id == self.sensor_ids[platform_handle]['sensor_id']) \
+                                            .where(multi_obs.m_date == xmrg_results_data.datetime)
 
+                                        self._xenia_db.connection.execute(db_update_rec)
+                                    except Exception as e:
+                                        self._logger.exception(e)
+                                    else:
+                                        self._logger.debug(
+                                            f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} updated "
+                                            f"weighted avg: {avg} in {time.time() - add_obs_start_time} seconds.")
+
+                            except Exception as e:
+                                self._logger.exception(e)
+
+                        else:
+                            self._logger.debug(
+                                f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} weighted avg: {avg}(mm)"
+                                f" is not valid, not adding to database.")
                     else:
                         self._logger.debug(
-                            f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} weighted avg: {avg}(mm)"
-                            f" is not valid, not adding to database.")
+                            f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} configuration parameter "
+                            f"not set to add precip values of 0.0.")
                 else:
-                    self._logger.debug(
-                        f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} configuration parameter "
-                        f"not set to add precip values of 0.0.")
-            else:
-                self._logger.error(f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} "
-                                   f"Weighted AVG error")
+                    self._logger.error(f"Platform: {platform_handle} Date: {xmrg_results_data.datetime} "
+                                       f"Weighted AVG error")
 
-        self._check_exists = False
+            self._check_exists = False
+        except Exception as e:
+            self._logger.exception(e)
         return
